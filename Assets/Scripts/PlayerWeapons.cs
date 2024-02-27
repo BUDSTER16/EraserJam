4c5dabd4dbb32705788d69f996100e060b17d0d0
BUDSTER16 <pwnagemm@hotmail.ca>
2024-02-27 11:36:29 -0500

    added AOE dmg circle

--- Assets/Scripts/PlayerWeapons.cs
+++ Assets/Scripts/PlayerWeapons.cs
@@ -11,13 +11,14 @@ public class PlayerWeapons : MonoBehaviour
     public float curseDamage = 0.2f;
     public float healMultiplier = 1.0f;
 
-    private Weapon circle = new Weapon("Circle", 5f, 0, 0, false);
+    [Header("Weapons")]
+    public GameObject eraseCircle;
 
-    private Weapon[] equipped = new Weapon[6];
+    private string[] equipped = { "def", "def", "def", "def", "def", "def" };
 
     void Start()
     {
-        Equip(circle);
+        Equip(eraseCircle);
     }
 
 
@@ -26,15 +27,16 @@ public class PlayerWeapons : MonoBehaviour
         
     }
 
-    void Equip(Weapon equWeapon)
+    void Equip(GameObject equWeapon)
     {
         bool complete = false;
         for (int i = 0; i < equipped.Length; i++)
         {
-            if (equipped[i].name != "def" && !complete)
+            if (equipped[i] == "def" && !complete)
             {
-                equipped[i] = equWeapon;
+                equipped[i] = equWeapon.GetComponent<AOEWeapon>().weaponName;
                 complete = true;
+                AttachWeapon(equWeapon);
             }
         }
         if (!complete) 
@@ -43,37 +45,9 @@ public class PlayerWeapons : MonoBehaviour
         }
     }
 
-    void CreateEffect(Weapon equWeapon)
+    void AttachWeapon(GameObject equWeapon)
     {
-
-    }
-}
-
-
-[System.Serializable]
-public class Weapon
-{
-    public string name;
-    public float damage;
-    public Boolean isCursed;
-    public float heal;
-    enum WeaponType {AOE, Proj, Orbit};
-    public short type;
-
-    public Weapon()
-    {
-        name = "def";
-        damage = 0.0f;
-        isCursed = false;
-        heal = 0.0f;
-        type = 0;
-    }
-    public Weapon(string weapName, float dmg, short useage, float regen, bool curse)
-    {
-        name = weapName;
-        damage = dmg;
-        type = useage;
-        heal = regen;
-        isCursed = curse;
+        GameObject newWeapon = Instantiate(equWeapon, this.transform);
+        newWeapon.transform.transform.localPosition = new Vector3(0, -25, 0);
     }
 }
